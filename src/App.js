ea0c06227ac77f1e36de73f6e2652c3dea5d4535
tertiux44 <secondsonofadebayo@gmail.com>
2023-05-09 21:02:06 +0100

    stopping here today zzzzz

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Settings from './Pages/Settings'
 import './index.scss'
 import LoginContext from './Contexts/LoginContext'
 import { useReducer } from 'react'
+import { useLayoutEffect } from 'react'
 
 function App() {
   const APPINITIAL = {
@@ -33,11 +34,19 @@ function App() {
   const updateApp = (state, action)=>{
     switch (action.target){
       case 'themeToggle':
+        setTimeout(()=>{
+          const appDataTemp = JSON.stringify(appData)
+          window.localStorage.setItem('storedTaskData', appDataTemp)
+        },500)
         return ({
           ...state,
           darkTheme: !state.darkTheme,
         })
         case 'addTask':
+          setTimeout(()=>{
+            const appDataTemp = JSON.stringify(appData)
+            window.localStorage.setItem('storedTaskData', appDataTemp)
+          },500)
           return ({
             ...state,
             tasksUnchecked:[
@@ -51,8 +60,12 @@ function App() {
                 }
               }
             ],
-        })
-        case 'checkTask':
+          })
+          case 'checkTask':
+          setTimeout(()=>{
+            const appDataTemp = JSON.stringify(appData)
+            window.localStorage.setItem('storedTaskData', appDataTemp)
+          },500)
           var filteredTasks = state.tasksUnchecked.filter((task)=>{
             return (task.taskId !== action.taskToCheck.taskId)
           })
@@ -77,6 +90,10 @@ function App() {
             ],
           })
           case 'unCheckTask':
+            setTimeout(()=>{
+              const appDataTemp = JSON.stringify(appData)
+              window.localStorage.setItem('storedTaskData', appDataTemp)
+            },500)
             var filteredTasksX = state.tasksChecked.filter((task)=>{
               return (task.taskId !== action.taskToUnCheck.taskId)
             })
@@ -101,12 +118,20 @@ function App() {
                 ...filteredTasksX
               ],
             })
-          case 'clearCompleted':
+            case 'clearCompleted':
+            setTimeout(()=>{
+              const appDataTemp = JSON.stringify(appData)
+              window.localStorage.setItem('storedTaskData', appDataTemp)
+            },500)
             return({
               ...state,
               tasksChecked: []
             })
-          case 'deleteTask':
+            case 'deleteTask':
+            setTimeout(()=>{
+              const appDataTemp = JSON.stringify(appData)
+              window.localStorage.setItem('storedTaskData', appDataTemp)
+            },500)
             // var thisTask = action.taskToDelete
             var thisTaskType = action.taskTypeToDelete
             let newList
@@ -132,14 +157,66 @@ function App() {
                 ]
               })
             }
-      default:
-        return({
-          ...state
-        })
+          case 'updateDataFromStorage':
+            var tobeRecievedString = window.localStorage.getItem('storedTaskData')
+            var tobeRecieved = JSON.parse(tobeRecievedString)
+            var optimizedChecked = tobeRecieved.tasksChecked.forEach((task)=>{
+              return (task.unCheckTask = (taskToUnCheck)=>{
+                dispatch({target: 'unCheckTask', taskToUnCheck})
+              })
+            })
+            var optimizedUnChecked = tobeRecieved.tasksUnchecked.forEach((task)=>{
+              return (task.checkTask = (taskToCheck)=>{
+                dispatch({target: 'checkTask', taskToCheck})
+              })
+            })
+            var optimizedFunctions = {
+              toggleTheme: ()=>{
+                dispatch({target: 'themeToggle'})
+              },
+              addTask: (taskToAdd)=>{
+                dispatch({target: 'addTask', taskToAdd})
+              },
+              deleteTask: (taskToDelete, taskTypeToDelete)=>{
+                dispatch({target: 'deleteTask', taskToDelete, taskTypeToDelete})
+              },
+              clearCompleted: ()=>{
+                dispatch({target: 'clearCompleted'})
+              }
+            }
+            console.log(tobeRecieved)
+            return({
+              ...state,
+              ...tobeRecieved,
+              ...optimizedChecked,
+              ...optimizedUnChecked,
+              ...optimizedFunctions,
+            })
+            
+            default:
+              return ({
+                ...state,
+      
+              })
     }    
   }
 
   const [appData, dispatch] = useReducer(updateApp, APPINITIAL)
+
+  useLayoutEffect(()=>{
+    const prevStorage = window.localStorage.getItem('storedTaskData')
+    if (prevStorage){
+      dispatch({target: 'updateDataFromStorage'})
+    } else{
+      window.localStorage.removeItem('storedTaskData')
+    }
+
+  }, [])
+
+  // useEffect(()=>{
+  //   const appDataTemp = JSON.stringify(appData)
+  //   window.localStorage.setItem('storedTaskData', appDataTemp)
+  // },[appData])
 
   return (
     <LoginContext.Provider value={appData}>
